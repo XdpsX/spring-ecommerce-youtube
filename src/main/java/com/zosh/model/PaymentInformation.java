2b36ac6ee3e830f1987446aad5bd3dac9e3ff058
Night <dranhata@gmail.com>
2023-10-04 20:58:57 +0700

    #5 Entity

---
+++ src/main/java/com/zosh/model/PaymentInformation.java
@@ -0,0 +1,11 @@
+package com.zosh.model;
+
+import java.time.LocalDate;
+
+public class PaymentInformation {
+
+    private String cardholderName;
+    private String cardNumber;
+    private LocalDate expirationDate;
+    private String cvv;
+}
---
+++ src/main/java/com/zosh/model/Rating.java
@@ -0,0 +1,26 @@
+package com.zosh.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="product_id", nullable = false)
+    @JsonIgnore
+    private Product product;
+
+    private double rating;
+    private LocalDateTime createdAt;
+}
